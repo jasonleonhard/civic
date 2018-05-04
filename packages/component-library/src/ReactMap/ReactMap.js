0a2139d53c3f0c5e48fc483ac4e5de3664185a2c
David <mendoza.line@aim.com>
2018-05-04 04:46:06 +0000

    Add Storybook knobs

--- packages/component-library/src/ReactMap/ReactMap.js
+++ packages/component-library/src/ReactMap/ReactMap.js
@@ -5,7 +5,7 @@ import isClient from '../utils/isClient';
 const ReactMap = ({ width, height, longitude, latitude, zoom, minZoom, maxZoom, mapboxStyle, mapboxToken}) => (
     <div>
       {isClient &&
-        <MapGL width={width} height={height} longitude={longitude} latitude={latitude}
+        <MapGL width={width} height={height} latitude={latitude} longitude={longitude}
           zoom={zoom} minZoom={minZoom} maxZoom={maxZoom} 
           mapStyle={mapboxStyle} mapboxApiAccessToken={mapboxToken} />
         }
@@ -15,8 +15,8 @@ const ReactMap = ({ width, height, longitude, latitude, zoom, minZoom, maxZoom, 
 ReactMap.propTypes = {
   width: PropTypes.number,
   height: PropTypes.number,
-  longitude: PropTypes.number,
   latitude: PropTypes.number,
+  longitude: PropTypes.number,
   zoom: PropTypes.number,
   minZoom: PropTypes.number,
   maxZoom: PropTypes.number,
@@ -27,13 +27,12 @@ ReactMap.propTypes = {
 ReactMap.defaultProps = {
   width: window.innerWidth,
   height: window.innerHeight,
-  longitude: -122.66,
-  latitude: 45.52,
+  latitude: 45.5237732,
+  longitude: -122.6622935,
   zoom: 12,
   minZoom: 10,
   maxZoom: 16,
   mapboxStyle: "mapbox://styles/themendozaline/cjg6296ub04ot2sqv9izku3qq",
-  mapboxToken: "",
 };
 
 ReactMap.displayName = "ReactMap";
--- packages/component-library/stories/ReactMap.story.js
+++ packages/component-library/stories/ReactMap.story.js
@@ -1,32 +1,66 @@
 import React from 'react';
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from '@storybook/react';
-
+import { withKnobs, number, selectV2 } from '@storybook/addon-knobs';
 import { ReactMap } from '../src';
 import '@hackoregon/component-library/assets/mapbox-gl.css';
 
-const styles = [
-  {name: 'Scenic', url: 'mapbox://styles/themendozaline/cj8rrlv4tbtgs2rqnyhckuqva'},
-  {name: 'Navigation Guidance Night', url: 'mapbox://styles/themendozaline/cj6y6f5m006ar2sobpimm7ay7'},
-  {name: 'label maker', url: 'mapbox://styles/themendozaline/cjg627xuw08mk2spjsb8jmho7'},
-];
-
 const displayName = ReactMap.displayName || 'ReactMap';
-const title = 'Simple usage';
-const description = `
-  This is a basic React-Map-GL map.`;
 
-const demoCode = () => (
-  <ReactMap
-    latitude={47.6062} longitude={-122.3321}
-    mapboxStyle={styles[2].url} />
-);
+const mapboxToken = 'pk.eyJ1IjoidGhlbWVuZG96YWxpbmUiLCJhIjoiY2o1aXdoem1vMWtpNDJ3bnpqaGF1bnlhNSJ9.sjTrNKLW9daDBIGvP3_W0w';
+
+const demoMap = () => {
+  const defaultLatitude = 45.5237732;
+  const optionsLatitude = {
+    range: true,
+    min: 25,
+    max: 50,
+    step: 0.0000001,
+  };
+  const latitude = number('Center Latitude (North/South)', defaultLatitude, optionsLatitude);
+
+  const defaultLongitude = -122.6622935;
+  const optionsLongitude = {
+    range: true,
+    min: -125,
+    max: -65,
+    step: 0.0000001,
+  };
+  const longitude = number('Center Longitude (West/East)', defaultLongitude, optionsLongitude);
+
+  const defaultZoom = 4;
+  const optionsZoom = {
+    range: false,
+    min: 1,
+    max: 15,
+    step: 1,
+  };
+  const zoom = number('Zoom Level', defaultZoom, optionsZoom);
+
+  const optionsStyle = {
+    'Lè Shine': 'mapbox://styles/themendozaline/cjg6296ub04ot2sqv9izku3qq',
+    'Label Maker': 'mapbox://styles/themendozaline/cjg627xuw08mk2spjsb8jmho7',
+    'Moonlight': 'mapbox://styles/themendozaline/cjgq6r2lg00072rmqj1wocgdq',
+    'Navigation Guidance Night': 'mapbox://styles/themendozaline/cj6y6f5m006ar2sobpimm7ay7',
+    'North Star': 'mapbox://styles/themendozaline/cj5oyewyy0fg22spetiv0hap0',
+    'Odyssey': 'mapbox://styles/themendozaline/cjgq6rklb000d2so1b8myaait',
+    'Scenic': 'mapbox://styles/themendozaline/cj8rrlv4tbtgs2rqnyhckuqva',
+  };
+  const mapboxStyle = selectV2('Mapbox Style', optionsStyle, optionsStyle['Label Maker']);
+
+  return (
+    <ReactMap
+      latitude={latitude} longitude={longitude} zoom={zoom}
+      mapboxToken={mapboxToken} mapboxStyle={mapboxStyle} />
+  );
+};
 
 export default () => storiesOf(displayName)
+  .addDecorator(withKnobs)
   .add(
-    title,
-    (demoCode)
+    'Simple usage',
+    (demoMap)
   ).add(
     'Default map', (() => (
-    <ReactMap />
+    <ReactMap mapboxToken={mapboxToken} />
   )));
